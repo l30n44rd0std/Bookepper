6053337c7ff4511253568b01c190587b4d685964
Leonardo José <118532015+l30n44rd0std@users.noreply.github.com>
2023-06-23 08:54:43 -0300

    Delete src/api directory

--- src/api/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const dbSequelize = new Sequelize( 'usuario', 'aluno.ifal', 'aluno.ifal', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = dbSequelize;
