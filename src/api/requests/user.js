0e8177cc0201d9910ccca82a9b50a72d55d4b4f5
l30n44rd0 <ljsf2@aluno.ifal.edu.br>
2023-09-05 20:29:09 -0300

    rotas, api para usuário

---
+++ src/api/requests/user.js
@@ -0,0 +1,32 @@
+const { default: axios } = require("axios");
+
+const apiUrl = "http://10.220.30.133:3005/user";
+
+const requestsUser = {
+    async login ({email, password}){
+        return await axios.post(apiUrl + '/login', {email: email, password: password})
+    },
+    async register ({username, email, password}){
+        return await axios.post(apiUrl + '/register', {username: username, email: email, password: password})
+    },
+    async update ({username, email, password}){
+        return await axios.post(apiUrl + '/:id/update', {username: username, email: email, password: password})
+    },
+    async loadProfile ({username, email}){
+        return await axios.post(apiUrl + '/:id/profile', {username: username, email: email})
+    },
+    async createFavBook (){
+        return await axios.post(apiUrl + '/:id/favbooks/create')
+    },
+    async showFavBooks (){
+        return await axios.post(apiUrl + '/:id/favBooks')
+    },
+    async updateStatusBook (){
+        return await axios.post(apiUrl + '/:id/favbooks/:id')
+    },
+    async deleteFavBook (){
+        return await axios.post(apiUrl + '/:id/favbooks/:idbook')
+    },
+}
+
+export default requestsUser;
