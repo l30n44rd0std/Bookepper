89084a4f5c149dcd9b6948f585b5da7beaf6e0bf
l30n44rd0 <ljsf2@aluno.ifal.edu.br>
2023-09-13 14:44:14 -0300

    add context, handleRegister com toast

--- src/pages/TelaCriarConta.js
+++ src/pages/TelaCriarConta.js
@@ -1,11 +1,75 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ToastAndroid,
+} from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+import requestsUser from "../api/requests/user";
+import { useUserContext } from '../UserContext';
 
 export default function TelaCriarConta() {
-  const [nome, setNome] = useState("");
+
+  const { updateUser } = useUserContext();
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigation = useNavigation();
+
+  function handleLoginScreen() {
+    navigation.navigate("TelaLogin");
+  }
+
+  const handleRegister = async () => {
+    try {
+      const response = await requestsUser.register({username: username, email: email, password: password});
+      const user = { username: response.data?.username, email: email, password: password }
+    
+      if (user.username == '' || user.email == '' || user.password == '') {
+        console.info('Nome de usuário, e-mail e/ou senha vazios.')
+        ToastAndroid.showWithGravityAndOffset(
+          'Nome, e-mail e/ou senha vazios.',
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM,
+          25,
+          50
+        );
+      } else {
+        updateUser(user);
+        console.info('Sucesso! Novo usuário registrado', response.data)
+        navigation.navigate('TelaLogin');
+  
+        ToastAndroid.showWithGravityAndOffset(
+          'Usuário criado com sucesso!',
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM,
+          25,
+          50
+        );
+      }
+    } catch (error) {
+      console.error(`Erro ao criar o novo usuario ${JSON.stringify(error)}`);
+      if (error.response) {
+        console.log('Data:', error.response.data);
+        console.log('Status:', error.response.status);
+        console.log('Headers:', error.response.headers);
+      }
+
+      ToastAndroid.showWithGravityAndOffset(
+        'Não foi possível registrar o usuário.',
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        25,
+        50
+      );
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -16,41 +80,51 @@ export default function TelaCriarConta() {
       </Text>
 
       <View style={styles.formulario}>
+        <Text style={{ color: "#fff", paddingBottom: 5 }}>Nome de usuário</Text>
         <TextInput
-          label="Nome"
           placeholder="Digite seu nome"
-          value={nome}
+          value={username}
           style={styles.input}
-          onChangeText={setNome}
+          onChangeText={setUsername}
         />
+
+        <Text style={{ color: "#fff", paddingBottom: 5 }}>E-mail</Text>
         <TextInput
-          label="E-mail"
           placeholder="Digite seu e-mail"
           value={email}
           style={styles.input}
           onChangeText={setEmail}
         />
+
+        <Text style={{ color: "#fff", paddingBottom: 5 }}>Senha</Text>
         <TextInput
-          label="Senha"
           placeholder="Digite sua senha"
-          value={senha}
+          value={password}
           secureTextEntry={true}
           style={styles.input}
-          onChangeText={setSenha}
+          onChangeText={setPassword}
         />
       </View>
 
-      <Button mode="contained" style={styles.botao}> //add onPress={}
-        Cadastrar
-      </Button>
+      <TouchableOpacity style={styles.btn} onPress={handleRegister}>
+        <Text style={{ color: "#fff", fontWeight: "bold" }}>Cadastrar</Text>
+      </TouchableOpacity>
+
+      <Text style={{ color: "#fff", marginTop: 30 }}>Já tem uma conta?</Text>
+      <Text
+        onPress={handleLoginScreen}
+        style={{ color: "#fff", fontWeight: "bold", textAlign: "center" }}
+      >
+        Entrar
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   icone: {
-    width: 100,
-    height: 100,
+    width: 150,
+    height: 150,
   },
   container: {
     flex: 1,
@@ -60,33 +134,37 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   titulo: {
-    fontSize: 32,
+    fontSize: 55,
     fontWeight: "bold",
-    marginBottom: 30,
+    marginBottom: 5,
     color: "#FFFFFF",
   },
   subtitulo: {
-    fontSize: 10,
+    fontSize: 15,
     opacity: 0.6,
     textAlign: "center",
-    marginBottom: 20,
+    marginBottom: 60,
     color: "#FFFFFF",
   },
   formulario: {
     width: "80%",
   },
   input: {
-    borderRadius: 5,
+    borderRadius: 10,
     marginBottom: 15,
     placeholder: "#1F1F1F",
     placeholderTextColor: "#1F1F1F",
     textColor: "#1F1F1F",
     color: "#1F1F1F",
     backgroundColor: "#7BAFE3",
+    // outlineStyle: '#1F1F1F'
+    fontSize: 16,
+    height: 40,
+    paddingLeft: 14,
   },
-  botao: {
+  btn: {
     uppercase: "",
-    color:"#4E0189",
+    color: "#4E0189",
     backgroundColor: "#104C87",
     borderRadius: 8,
     height: 50,
