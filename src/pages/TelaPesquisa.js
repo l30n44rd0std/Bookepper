4b0d9ac8d2653358383d9216a56a50e7abd79fd0
l30n44rd0 <ljsf2@aluno.ifal.edu.br>
2023-10-22 20:08:53 -0300

    flatlist, botao de limpar e livros pre definidos

--- src/pages/TelaPesquisa.js
+++ src/pages/TelaPesquisa.js
@@ -6,6 +6,7 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+  FlatList,
   StyleSheet,
 } from "react-native";
 import { Appbar, Searchbar } from "react-native-paper";
@@ -15,8 +16,13 @@ const TelaPesquisa = () => {
   const [query, setQuery] = useState("");
   const [bookData, setBookData] = useState(null);
   const [totalResults, setTotalResults] = useState(0);
+  const [showDefaultBooks, setShowDefaultBooks] = useState(true);
+
+  const maxLength = 30;
+  const authorMaxLength = 50;
 
   const searchBooks = () => {
+    setShowDefaultBooks(false);
     const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
 
     fetch(apiUrl)
@@ -41,6 +47,17 @@ const TelaPesquisa = () => {
     navigation.navigate("TelaDetalhesLivro", { book, googleId: book.id });
   };
 
+  const handleDefaultBookPress = (googleId) => {
+    navigation.navigate("TelaDetalhesLivro", googleId);
+  }
+  
+  const handleClearBtn = () => {
+    setQuery("");
+    setBookData(null);
+    setTotalResults(0);;
+    setShowDefaultBooks(true);
+  }
+  
   // const renderItem = ({ item }) => (
   //   <TouchableOpacity key={item.id} onPress={() => handleBookPress(item)}>
   //     <View style={{ marginBottom: 16 }}>
@@ -88,36 +105,175 @@ const TelaPesquisa = () => {
         <TouchableOpacity onPress={searchBooks} style={styles.btnPesquisar}>
           <Text style={{ color: "#fff", fontWeight: "bold" }}>Buscar</Text>
         </TouchableOpacity>
+        <TouchableOpacity onPress={handleClearBtn} style={[styles.btnPesquisar, {backgroundColor: '#FF0000', marginTop: 10}]}>
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Limpar</Text>
+        </TouchableOpacity>
 
-        <ScrollView style={styles.listaResultado} showsVerticalScrollIndicator={false}>
-          {bookData &&
-            bookData.map((item, index) => (
-              <TouchableOpacity key={index} onPress={() => handleBookPress(item)}>
-                <View style={{ marginBottom: 16 }}>
-                  {item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail ? (
-                    <Image
-                      source={{ uri: item.volumeInfo.imageLinks.thumbnail }}
-                      style={{ width: 200, height: 300, borderRadius: 15 }}
-                    />
-                  ) : (
-                    <Image
-                      source={require("../icons/imagem-de-capa-indisponivel.png")}
-                      style={{ width: 200, height: 300, borderRadius: 15 }}
-                    />
-                  )}
-                  <Text style={{ color: "#fff", fontWeight: "bold" }}>
-                    {item.volumeInfo.title}
-                  </Text>
-                  <Text style={{ color: "#fff" }}>
-                    {item.volumeInfo.authors?.join(", ")}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            ))}
-          {bookData && bookData.length === 0 && (
-            <Text style={{ color: "#fff" }}>Nenhum livro encontrado.</Text>
+        <FlatList
+          data={bookData}
+          keyExtractor={(item, index) => index.toString()}
+          vertical={true}
+          numColumns={2} // Defina o número de colunas como 3
+          contentContainerStyle={styles.listaResultado}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity key={index} onPress={() => handleBookPress(item)}>
+              <View style={{ marginBottom: 16 }}>
+                {item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail ? (
+                  <Image
+                    source={{ uri: item.volumeInfo.imageLinks.thumbnail }}
+                    style={{ width: 200, height: 300, borderRadius: 15 }}
+                  />
+                ) : (
+                  <Image
+                    source={require("../icons/imagem-de-capa-indisponivel.png")}
+                    style={{ width: 200, height: 300, borderRadius: 15 }}
+                  />
+                )}
+                <Text style={{ color: "#fff", fontWeight: "bold" }}>
+                  {item.volumeInfo.title}
+                </Text>
+                <Text style={{ color: "#fff" }}>
+                  {item.volumeInfo.authors?.join(", ")}
+                </Text>
+              </View>
+            </TouchableOpacity>
           )}
-        </ScrollView>
+        />          
+        {bookData && bookData.length === 0 && (
+          <Text style={{ color: "#fff" }}>Nenhum livro encontrado.</Text>
+        )}
+        {showDefaultBooks && (
+            <ScrollView>
+            <View style={styles.viewTopics}>
+              <View style={styles.viewTopic}>
+                <Text style={styles.topicName}>Autoajuda</Text>
+                <TouchableOpacity>
+                  <Text style={styles.verMais}>Ver mais</Text>
+                </TouchableOpacity>
+              </View>
+              <ScrollView horizontal>
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/mindset.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>Mindset</Text>
+                  <Text style={styles.nameAuthorTopic}>Carol S.Dweck</Text>
+                </View>
+
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/pairico.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>Pai Rico, Pai Pobre</Text>
+                  <Text style={styles.nameAuthorTopic}>Robert Kiyosaki</Text>
+                </View>
+
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/sutilarte.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>A Sutil Arte de Ligar o F*da-se</Text>
+                  <Text style={styles.nameAuthorTopic}>Mark Manson</Text>
+                </View>
+
+              </ScrollView>
+
+              <View style={[styles.viewTopic, {paddingTop: 30}]}>
+                <Text style={styles.topicName}>Romance</Text>
+                <TouchableOpacity> 
+                  <Text style={styles.verMais}>Ver mais</Text> 
+                </TouchableOpacity>
+              </View>
+              <ScrollView horizontal>
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/orgulho-e-preconceito.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>Orgulho e preconceito</Text>
+                  <Text style={styles.nameAuthorTopic}>Jane Austen</Text>
+                </View>
+
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/assim-que-comeca.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic} ellipsizeMode="tail" numberOfLines={1}>É assim que começa (Vol. 2 É assim que acaba)</Text>
+                  <Text style={styles.nameAuthorTopic}>Colleen Hoover</Text>
+                </View>
+
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/morro-dos-ventos-uivantes.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>O morro dos ventos uivantes</Text>
+                  <Text style={styles.nameAuthorTopic}>Emily Bronte</Text>
+                </View>
+
+              </ScrollView>
+
+              <View style={[styles.viewTopic, {paddingTop: 30}]}>
+                <Text style={styles.topicName}>Fantasia</Text>
+                <TouchableOpacity>
+                  <Text style={styles.verMais}>Ver mais</Text>
+                </TouchableOpacity>
+              </View>
+              <ScrollView horizontal>
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/senhor-dos-aneis.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>O Senhor dos Anéis: A Sociedade do Anel</Text>
+                  <Text style={styles.nameAuthorTopic}>J.R.R. Tolkien</Text>
+                </View>
+
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/harry-potter.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>Harry Potter e a Pedra Filosofal: 1</Text>
+                  <Text style={styles.nameAuthorTopic}>J.K. Rowling</Text>
+                </View>
+
+                <View style={styles.horizontalBook}>
+                  <TouchableOpacity onPress={ () => handleDefaultBookPress('aizjDQAAQBAJ')}>
+                    <Image
+                      source={require('../imgs_books/1984.jpg')}
+                      style={styles.imgBookTopics}
+                    />
+                  </TouchableOpacity>
+                  <Text style={styles.nameBookTopic}>1984</Text>
+                  <Text style={styles.nameAuthorTopic}>George Orwell</Text>
+                </View>
+
+              </ScrollView>
+
+            </View>
+            </ScrollView>
+        )}
       </View>
     </>
   );
@@ -155,6 +311,43 @@ const styles = StyleSheet.create({
     alignContent: "center",
     alignItems: "center",
   },
+  //-----------------------------------------
+  viewTopics: {
+    paddingLeft: 5,
+  },
+  viewTopic: {
+    flexDirection: "row", 
+    alignItems: "baseline", 
+    justifyContent:"space-between",
+    paddingBottom: 20,
+  },
+  topicName: {
+    color: '#ffff',
+    fontWeight: 'bold',
+    fontSize: 40,
+    paddingLeft: 10,
+  },
+  verMais: {
+    color: '#ffff',
+    fontWeight: "800"
+  },
+  imgBookTopics: {
+    width: 200,
+    height: 250,
+    borderRadius: 20
+  },
+  nameBookTopic: {
+    color: '#ffff',
+    fontWeight: "500",
+    fontSize: 15
+  },
+  nameAuthorTopic: {
+    color: '#ffff',
+    fontWeight: "200"
+  },
+  horizontalBook: {
+    paddingRight: 20
+  }
 });
 
 export default TelaPesquisa;
